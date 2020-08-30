ee1ddb8ded6e5282c65a713401c7187abd760b65
Tasos Grivas <tasos@openriskcalculator.com>
2020-08-30 00:00:02 +0000

    version 0.8.5

--- R/calcEADRegulatory.R
+++ R/calcEADRegulatory.R
@@ -1,8 +1,10 @@
-#' Calculates the Exposure-At-Default (EAD) based on the given regulatory framework. It supports the CEM, SA-CCR and IMM frameworks
+#' Calculates the Exposure-At-Default (EAD) based on the given regulatory framework. It supports the CEM, IMM and (simplified) SA-CCR frameworks
 #' @title Calculates the Exposure-At-Default (EAD)
 #' @param trades The full list of the Trade Objects
 #' @param framework Specifies the regulatory framework used in the calculations. It can take the values of 'IMM', 'CEM', 'SA-CCR'
-#' @param col The margin agreement with the counterparty
+#' @param sa_ccr_simplified (Optional) Specifies whether the standard SACCR or its simplified version or the OEM will be implemented. It can take the values of '', 'simplified', 'OEM'
+#' @param CSA The margin agreement with the counterparty
+#' @param collateral    The amount of collaterals currently exchanged with the counterparty
 #' @param EEE A vector containing the effective expected exposure against the counterparty
 #' @param time_points The timepoints that the analysis is performed on
 #' @return The Exposure-At-Default
@@ -10,7 +12,7 @@
 #' @author Tasos Grivas <tasos@@openriskcalculator.com>
 #'
 
-calcEAD = function(trades, framework, col,EEE,time_points)
+calcEADRegulatory = function(trades, framework, sa_ccr_simplified="", CSA, collateral, EEE, time_points)
 {
 
   if(framework=='CEM')
@@ -25,20 +27,37 @@ calcEAD = function(trades, framework, col,EEE,time_points)
       addon[i] = addon_table$FindIntervalValue(maturity_vector[i])
 
     addon_amount = (0.4 + 0.6*NGR)*sum(addon)
-    EAD = max(max(sum(MtM_Vector),0)+addon_amount-as.numeric(!is.null(col$IM_cpty))*col$IM_cpty,0)
+    EAD = max(max(sum(MtM_Vector),0)+addon_amount-as.numeric(!is.null(CSA$IM_cpty))*CSA$IM_cpty,0)
   } else if(framework=='SA-CCR')
   {
+    if(sa_ccr_simplified=='')
+    {
+      simplified = FALSE;
+      OEM        = FALSE;
+    }else if(sa_ccr_simplified=='simplified')
+    {
+      simplified = TRUE;
+      OEM        = FALSE;
+    }else if(sa_ccr_simplified=='OEM')
+    {
+      simplified = TRUE;
+      OEM        = TRUE;
+    }else
+    {
+      simplified = FALSE;
+      OEM        = FALSE;
+    }
     requireNamespace("SACCR")
-
-    current_collateral = Trading::Collateral(ID="col_1",csa_id="csa_1",Amount=0.03,type="VariationMargin")
-    MF = col$CalcMF()
+    
+    # calculating the maturity factor
+    MF = CSA$CalcMF(simplified = simplified)
     # calculating the add-on
     trades_tree = SACCR::CreateTradeGraph(trades)
-    trades_tree <- SACCR::CalcAddon(trades_tree, MF)
+    trades_tree <- SACCR::CalcAddon(trades_tree, MF, simplified = simplified, OEM = OEM)
     # calculating the RC and the V-c amount
-    rc_values <- SACCR::CalcRC(trades, col, list(current_collateral))
+    rc_values <- SACCR::CalcRC(trades, CSA, list(collateral), simplified = simplified)
     # calculating the PFE after multiplying the addon with a factor if V-C<0
-    PFE <- SACCR::CalcPFE(rc_values$V_C, trades_tree$addon)
+    PFE <- SACCR::CalcPFE(rc_values$V_C, trades_tree$addon, simplified = simplified)
     # calculating the Exposure-at-Default
     EAD <- SACCR::CalcEAD(rc_values$RC,PFE)
 
